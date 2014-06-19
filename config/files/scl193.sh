4795103f8ce098ca4adac4e0403327ea23ccb7f3
Prabin Deka <pdeka@thoughtworks.com>
2014-06-19 10:50:21 +1000

    Prabin - remove the files. Will keep the here docs for now

--- config/files/scl193.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Setup PATH, LD_LIBRARY_PATH and MANPATH for ruby-1.9
-export PATH=$(dirname `scl enable ruby193 "which ruby"`):$PATH
-export LD_LIBRARY_PATH=$(scl enable ruby193 "printenv LD_LIBRARY_PATH"):$LD_LIBRARY_PATH
-export MANPATH=$(scl enable ruby193 "printenv MANPATH"):$MANPATH
